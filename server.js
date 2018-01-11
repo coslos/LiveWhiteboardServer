e02ec43a4b064fbacbd55f4ebce360c8dfc419bb
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-01-11 21:23:02 +0530

    Basic Server
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

---
+++ server.js
@@ -0,0 +1,22 @@
+/**
+ * Created by bhavyaagg on 11/01/18.
+ */
+
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+function onConnection(socket) {
+  socket.on('drawing', (data) => {
+    console.log(data)
+    socket.broadcast.emit('drawing', data)
+  });
+}
+
+io.on('connection', onConnection);
+
+http.listen(port, () => console.log('listening on port ' + port));
